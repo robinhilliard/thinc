49da4e5e715be8b83068bdf3f162d665f04b7ba8
Robin Hilliard <robin@rocketboots.com>
2018-06-10 11:08:08 +1000

    Basic attribute diff tests passing

--- lib/virtual_dom/diff.ex
+++ lib/virtual_dom/diff.ex
@@ -1,17 +1,77 @@
 defmodule VirtualDOM.Diff do
   @moduledoc false
 
+  import Enum, only: [flat_map: 2]
 
   def diff(
-        {:vnode, _, _, _, _, node_hash, tree_hash},
-        {:vnode, _, _, _, _, node_hash, tree_hash}) do
+        a = {:vnode, _, _, _, _, _, _},
+        b = {:vnode, _, _, _, _, _, _}) do
+    diff([], a, b) # Prepend empty path
+    # TODO reverse path lists?
+  end
+
+
+  def diff(
+        _,
+        {:vnode, _, _, _, _, node_hash, children_hash},
+        {:vnode, _, _, _, _, node_hash, children_hash}) do
     []  # Both hashes match, no changes
   end
 
   def diff(
-        {:vnode, a_tag, a_id, a_attrs, _, a_node_hash, tree_hash},
-        {:vnode, b_tag, b_id, b_attrs, _, b_node_hash, tree_hash}) do
-    []
+        path,
+        {:vnode, "text", _, _, _, a_node_hash, children_hash},
+        {:vnode, "text", b_text, _, _, b_node_hash, children_hash}) when a_node_hash != b_node_hash do
+    [{:set_text, path, b_text}]  # Text is special case, we use id for text content
+  end
+
+  def diff(
+        path,
+        {:vnode, tag, id, a_attrs, _, a_node_hash, children_hash},
+        {:vnode, tag, id, b_attrs, _, b_node_hash, children_hash}) when a_node_hash != b_node_hash do
+    patch_attrs(ref(path, id), a_attrs, b_attrs)
+  end
+
+
+  defp ref(path, id) do
+    if id == :noid do
+      path        # Full path list
+    else
+      [id, :id]   # Use shortcut direct to id.
+                  # Paths are in reverse order for append performance.
+    end
+  end
+
+
+  defp patch_attrs(path, a_attrs, b_attrs) do
+    a_keys = MapSet.new(Map.keys(a_attrs))
+    b_keys = MapSet.new(Map.keys(b_attrs))
+
+    added_keys = MapSet.difference(b_keys, a_keys)
+    removed_keys = MapSet.difference(a_keys, b_keys)
+    same_keys = MapSet.intersection(a_keys, b_keys)
+
+    flat_map(
+      removed_keys,
+      fn key ->
+        [{:del_attr, path, key}]
+      end) ++
+
+    flat_map(
+      added_keys,
+      fn key ->
+        [{:set_attr, path, key, b_attrs[key]}]
+      end) ++
+
+    flat_map(
+      same_keys,
+      fn key ->
+        case  a_attrs[key] == b_attrs[key] do
+          true -> []
+          false -> [{:set_attr, path, key, b_attrs[key]}]
+        end
+      end)
+
   end
 
 
--- test/virtual_dom_diff_test.exs
+++ test/virtual_dom_diff_test.exs
@@ -6,7 +6,91 @@ defmodule VirtualDOMDiffTest do
 
   test "single html node identity" do
     a = vnode "html"
+    assert diff(a, a) == []
+  end
+
+
+  test "single text node identity" do
+    a = view do
+      text "identity"
+    end
 
     assert diff(a, a) == []
   end
+
+
+  test "single text node replace" do
+    a = view do
+      text "bill"
+    end
+
+    b = view do
+      text "ben"
+    end
+
+    assert diff(a, b) == [{:set_text, [], "ben"}]
+  end
+
+
+  test "single node with attribute identity" do
+    a = view do
+      div class: "identity"
+    end
+
+    assert diff(a, a) == []
+  end
+
+
+  test "single attr replace" do
+    a = view do
+      div class: "bill"
+    end
+
+    b = view do
+      div class: "ben"
+    end
+
+    assert diff(a, b) == [{:set_attr, [], :class, "ben"}]
+  end
+
+
+    test "single attr add" do
+    a = view do
+      div
+    end
+
+    b = view do
+      div class: "ben"
+    end
+
+    assert diff(a, b) == [{:set_attr, [], :class, "ben"}]
+  end
+
+
+  test "single attr remove" do
+    a = view do
+      div class: "bill"
+    end
+
+    b = view do
+      div
+    end
+
+    assert diff(a, b) == [{:del_attr, [], :class}]
+  end
+
+
+  test "multiple attr replace" do
+    a = view do
+      img class: "a_class", src: "a_src"
+    end
+
+    b = view do
+      img class: "b_class", src: "b_src"
+    end
+
+    assert diff(a, b) ==
+      [{:set_attr, [], :class, "b_class"}, {:set_attr, [], :src, "b_src"}]
+  end
+
 end
